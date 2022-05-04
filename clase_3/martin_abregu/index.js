31740401bea1555ad92fe367ac5b41fb93db701a
N. Martin Abregu <nmabregu@gmail.com>
2022-05-04 20:12:28 -0300

    first commit 3era clase

---
+++ clase_3/martin_abregu/index.js
@@ -0,0 +1,7 @@
+let martin = [
+    "Martin Abregu",
+    "Escuchar musica",
+    "Punta Lara"
+]
+
+module.exports = martin;
---
+++ clase_3/series/index.js
@@ -0,0 +1,16 @@
+const series = [
+    {
+        name: 'Breaking Bad',
+        genre: 'Drama',
+    },
+    {
+        name: 'The Sopranos',
+        genre: 'Drama',
+    },
+    {
+        name: 'The IT Crowd',
+        genre: 'Comedy',
+    }
+];
+
+module.exports = series;
