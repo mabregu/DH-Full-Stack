264c2730d5c27b6fbccd6b6ed30f1d1e3b0c8f9d
Martin Abregu <nmabregu@gmail.com>
2022-05-31 21:00:28 -0300

    clase 11 part 2

--- clase_11/app.js
+++ clase_11/app.js
@@ -2,7 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, 'public'))
+);
 
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
